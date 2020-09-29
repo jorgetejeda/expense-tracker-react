65f8721b15b49c2b0a1be55829083ecd25274e8c
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-09-29 19:44:41 -0400

    Create context to handler global information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
-
 import Header from "./components/Header";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
+import "./App.css";
 
 function App() {
   return (
