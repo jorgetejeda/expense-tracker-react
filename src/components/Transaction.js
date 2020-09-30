676820ff30427d9292db374fac6d20a013e36b7a
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-09-29 20:45:40 -0400

    Created our first action type to delete a register

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
-export default function Transaction({transaction}) {
+export default function Transaction({ transaction }) {
+  const { deleteTransactions } = useContext(GlobalContext);
   const { text, amount, id } = transaction;
   return (
     <li className={amount < 0 ? "minus" : "plus"}>
       {text} <span>{amount}</span>
-      <button className="delete-btn">x</button>
+      <button className="delete-btn" onClick={()=>deleteTransactions(id)}>
+        x
+      </button>
     </li>
   );
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,8 +18,15 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //Actions, Call Reducers
+  function deleteTransactions(id) {
+    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{ transactions: state.transactions, deleteTransactions }}
+    >
       {children}
     </GlobalContext.Provider>
   );
